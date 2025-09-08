6cdbb32b3f1a46cb9d64a50d85103b0eddeff2bb
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-09-08 16:20:18 +0600

    google login done 100%

--- my-project/src/Components/MainLayout/Login.jsx
+++ my-project/src/Components/MainLayout/Login.jsx
@@ -1,10 +1,27 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import auth from '../../../firbase.init';
 
 const Login = () => {
+
+    const provider= new GoogleAuthProvider();
+
+    const handleGoogleLogin=()=>{
+        signInWithPopup(auth, provider)
+        .then(result=>{
+            console.log(result)
+        })
+        .catch(err=>{
+            console.log('Error',err)
+        })
+    }
+
+
+
     return (
         <div className='space-y-4'>
             <h1>This is Login Form</h1>
-            <button className='btn btn-info'>Google Login</button>
+            <button onClick={handleGoogleLogin} className='btn btn-info'>Google Login</button>
         </div>
     );
 };
